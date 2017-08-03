6819e5a588022a96f15f677d6f5495d60b3506c5
rakeshreddy <rakesh.kps99@gmail.com>
2017-08-03 08:09:52 -0500

    initial commit

---
+++ Packages/src/com/rakesh/Main.java
@@ -0,0 +1,13 @@
+package com.rakesh;
+
+import javafx.scene.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Node node = null;
+        // the below one is using fully qualified name as there is already an import for Node
+        org.w3c.dom.Node anotherNode = null;
+    }
+}
